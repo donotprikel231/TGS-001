dcdd2d990fd9231a171e795e155e5bf00582fe4c
donotprikel231 <118140095+donotprikel231@users.noreply.github.com>
2023-05-05 22:53:57 -0700

    Update scripts.js

---
+++ Assets/scripts.js
@@ -0,0 +1,4 @@
+const downloadGamesButton = document.getElementById('download-games-button');
+downloadGamesButton.addEventListener('click', () => {
+  window.location.href = 'Download.htm'; // Replace with the URL you want to open
+});
