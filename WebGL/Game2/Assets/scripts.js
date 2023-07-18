057e086890925e0d5eef1c84e2b7bb72f33db8f1
donotprikel231 <118140095+donotprikel231@users.noreply.github.com>
2023-07-17 17:20:38 -0700

    Add files via upload

---
+++ WebGL/Game2/Assets/scripts.js
@@ -0,0 +1,4 @@
+const moreGamesButton = document.getElementById('more-games-button');
+moreGamesButton.addEventListener('click', () => {
+  window.location.href = 'https://donotprikel231.github.io/TGS-001/'; // Replace with the URL you want to open
+});
